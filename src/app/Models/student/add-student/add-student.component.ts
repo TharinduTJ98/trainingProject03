f6fb18c6b4cdc50f9d1c96ec6e19d9f0eb002903
Tharindu Jayawardhana <tharindujayawardha1@gmail.com>
2025-06-13 16:06:29 +0530

    set student data throgh student service

--- src/app/Models/student/add-student/add-student.component.ts
+++ src/app/Models/student/add-student/add-student.component.ts
@@ -1,20 +1,25 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { StudentService } from '../Service/student.service';
+import { Student } from '../Modules/Student';
+
 @Component({
   selector: 'app-add-student',
   templateUrl: './add-student.component.html',
   styleUrls: ['./add-student.component.scss']
 })
 export class AddStudentComponent {
-  student = {
+  student: Student = {
+    id: 0,
     name: '',
     email: '',
     phone: ''
   };
-  constructor(private router: Router){
+  constructor(private router: Router, private studentService: StudentService){
 
   }
   onsubmit(): void{
+      this.studentService.addStudent(this.student);
       this.router.navigate(['/private/student/show-students']);
       console.log("Student data", this.student)
   }
--- src/app/Models/student/show-students/show-students.component.ts
+++ src/app/Models/student/show-students/show-students.component.ts
@@ -1,20 +1,21 @@
-import { Component } from '@angular/core';
-interface Student {
-  id: number;
-  name: string;
-  email: string;
-  phone?: string;
-}
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from '../Service/student.service';
+import { Student } from '../Modules/Student';
 @Component({
   selector: 'app-show-students',
   templateUrl: './show-students.component.html',
   styleUrls: ['./show-students.component.scss']
 })
-export class ShowStudentsComponent {
-  students: Student[] = [
-    {id:1, name:'ABC', email:'abc@gmail.com', phone:'077-6284276'},
-    {id:2, name:'DEF', email:'def@gmail.com', phone:'077-4579852'},
-    {id:3, name:'GHI', email:'ghi@gmail.com', phone:'077-4793584'},
-    {id:4, name:'JKL', email:'jkl@gmail.com', phone:'077-1498752'},
-  ];
+export class ShowStudentsComponent implements OnInit{
+  students: Student[] = [];
+  constructor(private studentSerive: StudentService){
+
+  }
+  ngOnInit(): void {
+    this.studentSerive.students$.subscribe(
+      data => {
+        this.students = data;
+      }
+    )
+  }
 }
